07a4d316e06a26a7e36274d61061731275a28f8a
abdulsagheer <abdulsagheeras29@gmail.com>
2022-12-29 17:29:55 +0530

    Implemented scroll view

---
+++ components/auth/CircleLogo.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Image, Text } from "react-native";
+
+const CircleLogo = () => {
+	return (
+		<View
+			style={{
+				justifyContent: "center",
+				alignItems: "center",
+			}}>
+			<Image
+				source={require("../../assets/logo.png")}
+				style={{ width: 300, height: 110, marginVertical: 30 }}
+			/>
+		</View>
+	);
+};
+
+export default CircleLogo;
--- screens/Signup.js
+++ screens/Signup.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
-import { View, TextInput } from "react-native";
+import { View, ScrollView } from "react-native";
 import Text from "@kaloraat/react-native-text";
 import UserInput from "../components/auth/UserInput";
+import SubmitButton from "../components/auth/SubmitButton";
+import axios from "axios";
+import CircleLogo from "../components/auth/CircleLogo";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const Signup = () => {
 	const [name, setName] = useState("");
@@ -9,36 +13,66 @@ const Signup = () => {
 	const [password, setPassword] = useState("");
 	const [loading, setLoading] = useState(false);
 
+	const handleSubmit = async () => {
+		setLoading(true);
+		if (!name || !email || !password) {
+			alert("All fields are required!!");
+			setLoading(false);
+			return;
+		}
+		console.log("Sign up request", { name, email, password });
+		try {
+			const { data } = await axios.post("http://localhost:8000/api/signup", {
+				name,
+				email,
+				password,
+			});
+			setLoading(false);
+			console.log("Signup successfully!!!!", data);
+			alert("Signup successfully!!!!");
+		} catch (error) {
+			console.log(error);
+			setLoading(false);
+		}
+	};
+
 	return (
-		<View style={{ flex: 1, justifyContent: "center" }}>
-			<Text title center>
-				Sign Up
-			</Text>
+		<KeyboardAwareScrollView
+			contentContainerStyle={{ flex: 1, justifyContent: "center" }}>
+			<View style={{ marginVertical: 100 }}>
+				<CircleLogo />
+				<Text title center>
+					Sign Up
+				</Text>
 
-			<UserInput
-				name="NAME"
-				value={name}
-				setValue={setName}
-				autoCapitalize="words"
-				autoCorrect={false}
-			/>
-			<UserInput
-				name="EMAIL"
-				value={email}
-				setValue={setEmail}
-				autoCompleteType="email"
-				keyboardType="email-address"
-			/>
-			<UserInput
-				name="PASSWORD"
-				value={password}
-				setValue={setPassword}
-				secureTextEntry={true}
-				autoComplteType="password"
-			/>
-
-			<Text>{JSON.stringify({ name, email, password }, null, 4)}</Text>
-		</View>
+				<UserInput
+					name="NAME"
+					value={name}
+					setValue={setName}
+					autoCapitalize="words"
+					autoCorrect={false}
+				/>
+				<UserInput
+					name="EMAIL"
+					value={email}
+					setValue={setEmail}
+					autoCompleteType="email"
+					keyboardType="email-address"
+				/>
+				<UserInput
+					name="PASSWORD"
+					value={password}
+					setValue={setPassword}
+					secureTextEntry={true}
+					autoComplteType="password"
+				/>
+				<SubmitButton
+					title="Sign Up"
+					handleSubmit={handleSubmit}
+					loading={loading}
+				/>
+			</View>
+		</KeyboardAwareScrollView>
 	);
 };
 
